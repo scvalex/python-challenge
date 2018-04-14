8f09e9e3624e91964e47382fbffa928386034e82
Alexandru Scvortov <code@scvalex.net>
2018-04-14 12:25:09 +0100

    solve first task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args();
+    match args.nth(1)
+        .unwrap_or(String::from("missing argument"))
+        .as_ref()
+    {
+        "0" => println!("{}", (2 as i64).pow(38)),
+        s => {
+            eprintln!("unknown task '{}'", s);
+            std::process::exit(1)
+        }
+    }
 }
