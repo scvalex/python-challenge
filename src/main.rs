730810865f2f0a0b1cdaefc08f975c00cbfa49ba
Alexandru Scvortov <code@scvalex.net>
2018-04-14 12:51:24 +0100

    clippified

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut args = std::env::args();
     match args.nth(1)
-        .unwrap_or(String::from("missing argument"))
+        .unwrap_or_else(|| String::from("missing argument"))
         .as_ref()
     {
         "0" => println!("{}", (2 as i64).pow(38)),
