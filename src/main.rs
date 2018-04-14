7c4b9e19e1ea92ec5484a1a24b0851e95d2c3149
Alexandru Scvortov <code@scvalex.net>
2018-04-14 12:41:43 +0100

    solve task 1

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ fn main() {
         .as_ref()
     {
         "0" => println!("{}", (2 as i64).pow(38)),
+        "1" => {
+            // let encoded = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. ";
+            let encoded = "map";
+            let decoded = String::from_utf8(
+                encoded
+                    .as_bytes()
+                    .iter()
+                    .map(|&ch| {
+                        if (b'a' <= ch) && (ch <= b'z') {
+                            ((ch - b'a') + 2) % 26 + b'a'
+                        } else {
+                            ch
+                        }
+                    })
+                    .collect::<Vec<u8>>(),
+            ).unwrap();
+            println!("{}", decoded)
+        }
         s => {
             eprintln!("unknown task '{}'", s);
             std::process::exit(1)
